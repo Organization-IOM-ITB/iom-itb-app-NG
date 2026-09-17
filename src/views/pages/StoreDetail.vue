b7c7f20ad2815dd68837f3133e3e021e87d85d32
A Z <madzul@madz-n-cya.com>
2026-09-17 11:13:31 +0700

    reenable opsi online payment

--- src/views/pages/StoreDetail.vue
+++ src/views/pages/StoreDetail.vue
@@ -118,7 +118,7 @@
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-main focus:border-main sm:text-sm"
         >
           <option value="manual">Manual (Transfer Bank)</option>
-          <option value="midtrans" disabled>Online (Midtrans) - Sementara Tidak Tersedia</option>
+          <option value="midtrans">Online (Midtrans)</option>
         </select>
         <span class="block text-sm font-medium text-gray-500 mt-2">
           Total Pembayaran: {{ formatPrice(quantity * currentMerchandise.price) }}
